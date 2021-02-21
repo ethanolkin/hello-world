a58a6df110ab144b63b6d4bf0ccad9de43b02bbe
Emil Popov <67501946+ethanolkin@users.noreply.github.com>
2021-02-21 17:42:32 +0300

    Add files via upload

---
+++ kek.c
@@ -0,0 +1,25 @@
+https://cdn.intra.42.fr/video/video/29/low_d_piscine-C-J04-2-1080p.mp4 Récursivité - Pratique
+https://cdn.intra.42.fr/video/video/28/low_d_piscine-C-J04-1-1080p.mp4 Récursivité - Théorie
+https://cdn.intra.42.fr/video/video/30/low_d_piscine-c-j05-0-1080p.mp4 Manipulation des chaines de caractères
+https://cdn.intra.42.fr/video/video/31/low_d_piscine-C-J06-0-1080p.mp4 Introduction
+https://cdn.intra.42.fr/video/video/33/low_d_piscine-C-J08-1.mp4 Préprocesseur - #include
+https://cdn.intra.42.fr/video/video/34/low_d_piscine-C-J08-0.mp4 Préprocesseur - Introduction
+https://cdn.intra.42.fr/video/video/35/low_d_piscine-C-J08-3.mp4 Préprocesseur - #define
+https://cdn.intra.42.fr/video/video/36/low_d_piscine-C-J08-4.mp4 Préprocesseur - #ifdef / #ifndef
+https://cdn.intra.42.fr/video/video/38/low_d_piscine-C-J08-7.mp4 Structures de données - Les structures
+https://cdn.intra.42.fr/video/video/39/low_d_piscine-C-J08-8.mp4 Structures de données - Les enums
+https://cdn.intra.42.fr/video/video/40/low_d_piscine-C-J08-9.mp4 Structures de données - Les unions
+https://cdn.intra.42.fr/video/video/41/low_d_piscine-C-J08-2.mp4 Préprocesseur - Les fichiers.h
+https://cdn.intra.42.fr/video/video/43/low_d_piscine-C-J10-6.mp4 Pointeurs sur fonction - Introduction
+https://cdn.intra.42.fr/video/video/44/low_d_piscine-C-J10-9.mp4 Pointeurs sur fonction - Conclusion
+https://cdn.intra.42.fr/video/video/45/low_d_piscine-C-J10-8.mp4 Pointeurs sur fonction - Exemple
+https://cdn.intra.42.fr/video/video/46/low_d_piscine-C-J10-1.mp4 Problématique de compilation
+https://cdn.intra.42.fr/video/video/47/low_d_piscine-C-J10-0.mp4 Introduction au j10
+https://cdn.intra.42.fr/video/video/48/low_d_piscine-C-J07-1.mp4 Allocation dynamique de mémoire
+https://cdn.intra.42.fr/video/video/50/low_d_piscine-C-J10-3.mp4 Makefile - Les règles
+https://cdn.intra.42.fr/video/video/51/low_d_piscine-C-J10-7.mp4 Pointeurs sur fonction - Syntaxe
+https://cdn.intra.42.fr/video/video/52/low_d_piscine-C-J10-5.mp4 Makefile - Exemple
+https://cdn.intra.42.fr/video/video/53/low_d_piscine-C-J10-4_new.mp4 Makefile - Les variables
+https://cdn.intra.42.fr/video/video/54/low_d_piscine-C-J02-0-1080p.mp4 Premiers pas en C
+https://cdn.intra.42.fr/video/video/55/low_d_piscine-C-J06-1.mp4 Les librairies
+https://cdn.intra.42.fr/video/video/56/low_d_piscine-C-J06-2-1080p.mp4 Les paramètres (argv/argc)
