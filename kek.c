a2f3c4ff450a8fe7fe0fc07d62896701963b105c
Emil Popov <67501946+ethanolkin@users.noreply.github.com>
2021-02-23 15:03:22 +0300

    Update kek.c

--- kek.c
+++ kek.c
@@ -73,9 +73,31 @@ https://cdn.intra.42.fr/video/video/469/low_d_BootCamp-d08-8-1080p.mp4 Data stru
 https://cdn.intra.42.fr/video/video/472/low_d_BootCamp-d08-9-1080p.mp4 Data structures - Union
 https://cdn.intra.42.fr/video/video/475/low_d_BootCamp-d10-0-1080p.mp4 Day 10 Introduction
 https://cdn.intra.42.fr/video/video/476/low_d_BootCamp-d10-1-1080p.mp4 Compilation Problematic
-
-
-
+https://cdn.intra.42.fr/video/video/477/low_d_BootCamp-d10-2-1080p.mp4 Makefile - Introduction
+https://cdn.intra.42.fr/video/video/478/low_d_BootCamp-d10-3-1080p.mp4 Makefile - Rules
+https://cdn.intra.42.fr/video/video/479/low_d_BootCamp-d10-4-1080p.mp4 Makefile - Variables
+https://cdn.intra.42.fr/video/video/480/low_d_BootCamp-d10-5-1080p.mp4 Makefile - Example
+https://cdn.intra.42.fr/video/video/481/low_d_BootCamp-d11-0-1080p.mp4 Chained list - Introduction
+https://cdn.intra.42.fr/video/video/482/low_d_BootCamp-d11-1-1080p.mp4 Chained list - Examples
+https://cdn.intra.42.fr/video/video/483/low_d_BootCamp-d11-2-1080p.mp4 Chained list - Tips
+https://cdn.intra.42.fr/video/video/484/low_d_BootCamp-d10-6-1080p.mp4.mp4 Pointers to functions - Introduction
+https://cdn.intra.42.fr/video/video/485/low_d_BootCamp-d10-7-1080p.mp4 Pointers to functions - Syntax
+https://cdn.intra.42.fr/video/video/486/low_d_BootCamp-d10-8-1080p.mp4 Pointers to functions - Example
+https://cdn.intra.42.fr/video/video/487/low_d_BootCamp-d10-9-1080p.mp4 Pointers to functions - Conclusion
+https://cdn.intra.42.fr/video/video/488/low_d_BootCamp-d12-0-1080p.mp4 Files - Introduction
+https://cdn.intra.42.fr/video/video/489/low_d_BootCamp-d12-1-1080p.mp4 File manipulation - Open
+https://cdn.intra.42.fr/video/video/490/low_d_BootCamp-d12-2-1080p.mp4 File manipulation - Write
+https://cdn.intra.42.fr/video/video/491/low_d_BootCamp-d12-3-1080p.mp4 File manipulation - Read
+https://cdn.intra.42.fr/video/video/492/low_d_BootCamp-d12-4-1080p.mp4 File manipulation - Lseek
+https://cdn.intra.42.fr/video/video/493/low_d_BootCamp-d13-0-1080p.mp4 Binary trees - Introduction
+https://cdn.intra.42.fr/video/video/494/low_d_BootCamp-d13-1-1080p.mp4 Binary trees - Usage
+https://cdn.intra.42.fr/video/video/495/low_d_BootCamp-d13-2-1080p.mp4 Binary trees - Balancing
+https://cdn.intra.42.fr/video/video/496/low_d_piscine_C_-_J03_-_0.mp4 Lieux communs sur les pointeurs
+https://cdn.intra.42.fr/video/video/498/low_d_piscine_C_-_J03_-_1.mp4 Assignation
+https://cdn.intra.42.fr/video/video/499/low_d_piscine_C_-_J03_-_2.mp4 Déréférencer
+https://cdn.intra.42.fr/video/video/500/low_d_piscine_C_-_J03_-_3.mp4 Arithmétique des pointeurs
+https://cdn.intra.42.fr/video/video/501/low_d_piscine_C_-_J03_-_4.mp4 Les tableaux
+https://cdn.intra.42.fr/video/video/502/low_d_piscine_C_-_J03_-_5.mp4 Les chaines de caractères
 
 
 
