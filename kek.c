0b93ce89775f62e51d4f55d26b13d9bf60b7e39a
Emil Popov <67501946+ethanolkin@users.noreply.github.com>
2021-02-23 14:29:15 +0300

    Update kek.c

--- kek.c
+++ kek.c
@@ -23,3 +23,31 @@ https://cdn.intra.42.fr/video/video/53/low_d_piscine-C-J10-4_new.mp4 Makefile - 
 https://cdn.intra.42.fr/video/video/54/low_d_piscine-C-J02-0-1080p.mp4 Premiers pas en C
 https://cdn.intra.42.fr/video/video/55/low_d_piscine-C-J06-1.mp4 Les librairies
 https://cdn.intra.42.fr/video/video/56/low_d_piscine-C-J06-2-1080p.mp4 Les paramètres (argv/argc)
+https://cdn.intra.42.fr/video/video/58/low_d_piscine-C-J13-0.mp4 Les arbres binaires - Intro
+https://cdn.intra.42.fr/video/video/59/low_d_piscine-C-J13-1-REAL-ONE.mp4 Utilisation
+https://cdn.intra.42.fr/video/video/60/low_d_piscine-C-J13-2.mp4 Equilibrage
+https://cdn.intra.42.fr/video/video/62/low_d_piscine-C-J12-1-new.mp4 Manipulation de fichier - open
+https://cdn.intra.42.fr/video/video/63/low_d_piscine-C-J11-0.mp4 Les listes chaînées - Introduction
+https://cdn.intra.42.fr/video/video/64/low_d_piscine-C-J12-2.mp4 Manipulation de fichier - write
+https://cdn.intra.42.fr/video/video/65/low_d_piscine-C-J12-4.mp4 Manipulation de fichier - lseek
+https://cdn.intra.42.fr/video/video/66/low_d_piscine-C-J12-3.mp4 Manipulation de fichier - read
+https://cdn.intra.42.fr/video/video/67/low_d_piscine-C-J11-2.mp4 Pointeurs sur pointeurs
+https://cdn.intra.42.fr/video/video/68/low_d_piscine-C-J11-1.mp4 Exemple pratique
+https://cdn.intra.42.fr/video/video/77/low_d_piscine-C-J07-0.mp4 Les étapes de compilation
+https://cdn.intra.42.fr/video/video/381/low_d_documentation-Cen42min-EN.mp4 Le C en 42 minutes
+https://cdn.intra.42.fr/video/video/407/low_d_BootCamp-d00-0-1080p.mp4 Basic commands
+https://cdn.intra.42.fr/video/video/410/low_d_BootCamp-d00-1-1080p.mp4 Filesystem
+https://cdn.intra.42.fr/video/video/411/low_d_BootCamp-d00-2-1080p.mp4 touch, cat
+https://cdn.intra.42.fr/video/video/425/low_d_BootCamp-d01-0-1080p.mp4 echo, cat, more
+https://cdn.intra.42.fr/video/video/426/low_d_BootCamp-d01-1-1080p.mp4 head, tail, grep
+https://cdn.intra.42.fr/video/video/427/low_d_BootCamp-d01-2-1080p.mp4 Redirections
+https://cdn.intra.42.fr/video/video/428/low_d_BootCamp-d01-3-1080p.mp4 sort, cut
+https://cdn.intra.42.fr/video/video/429/low_d_BootCamp-d01-4-1080p.mp4 wc, ifconfig, bc, find, env, export
+https://cdn.intra.42.fr/video/video/430/low_d_BootCamp-d01-5-1080p.mp4 stdout, stderr
+https://cdn.intra.42.fr/video/video/434/low_d_BootCamp-d02-0-1080p.mp4 First step in C
+https://cdn.intra.42.fr/video/video/437/low_d_BootCamp-d03-0-1080p.mp4 Introduction to pointers
+https://cdn.intra.42.fr/video/video/438/low_d_BootCamp-d03-1-1080p.mp4 Assignment
+https://cdn.intra.42.fr/video/video/439/low_d_BootCamp-d03-2-1080p.mp4 Dereferenciation
+
+
+
